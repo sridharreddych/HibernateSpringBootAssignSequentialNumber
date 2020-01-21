65cb08589ec041167730b780a7f05a4a5688eec7
SridharChallabotla <sridhar.challabotla@metlife.com>
2020-01-21 15:28:54 +0530

    first commit

---
+++ src/main/java/com/bookstore/service/BookstoreService.java
@@ -0,0 +1,22 @@
+package com.bookstore.service;
+
+import com.bookstore.dto.AuthorDto;
+import com.bookstore.repository.AuthorRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookstoreService {
+
+    private final AuthorRepository authorRepository;
+
+    public BookstoreService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public void fetchAuthorsWithSeqNumber() {
+        List<AuthorDto> authors = authorRepository.fetchWithSeqNumber();
+        
+        authors.forEach(a -> System.out.println( a.getName() + ", " + a.getName()));
+    }
+}
